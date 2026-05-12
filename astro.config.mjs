42281c5fb35ca9dd0608250f298fe82e9c323a93
trabajosoffice549-ui <trabajosoffice549@gmail.com>
2026-05-12 09:27:17 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,7 +58,7 @@ function rehypeBasePaths() {
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.ASTRO_SITE ?? 'https://example.github.io', // override via ASTRO_SITE env var or edit directly
+  site: process.env.ASTRO_SITE ?? 'https://trabajosoffice549.github.io', // override via ASTRO_SITE env var or edit directly
   base: process.env.ASTRO_BASE ?? '', // override via ASTRO_BASE env var or set '/repo-name' for project pages
   output: 'static',
   trailingSlash: 'always',
